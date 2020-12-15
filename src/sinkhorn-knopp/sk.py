9d646955dd5124040ae73d343106d0b168feb215
VladislavZh <48725475+VladislavZh@users.noreply.github.com>
2020-12-15 19:26:14 +0300

    fixed sk

--- src/sinkhorn-knopp/sk.py
+++ src/sinkhorn-knopp/sk.py
@@ -16,19 +16,19 @@ def sk_optimal_transport(P, r, c, N, K, lmbd, eps):
     return torch.diag(alpha) @ tmp @ torch.diag(beta)
 
 def sinkhorn(P, r, c, lmbd=0.05, eps = 1e-3):
-    #has bug, to be tested
+    P = -P
     Q = torch.exp(- lmbd * P).T
     Q /= torch.sum(Q)
     K, N = Q.shape
     u = torch.zeros(K)
     res = []
-    res.append(torch.sum(torch.trace(Q.T @ P.T)))
+    res.append(-torch.sum(torch.trace(Q.T @ P.T)))
     while True:
         Q_prev = Q.clone()
         u = torch.sum(Q, dim=1)
         Q *= (c / u).unsqueeze(1)
         Q *= (r / torch.sum(Q, dim=0)).unsqueeze(0)
-        res.append(torch.sum(torch.trace(Q.T @ P.T)))
+        res.append(-torch.sum(torch.trace(Q.T @ P.T)))
         if torch.sum(((Q-Q_prev)/Q_prev)**2) <eps:
             break
     return Q.T, res
