ae08d1908fe5ffa8b6560b3a7e108f130c72a310
VladislavZh <48725475+VladislavZh@users.noreply.github.com>
2020-12-16 22:48:28 +0300

    ot_2

--- src/optimal_transport/data_preprocessing.py
+++ src/optimal_transport/data_preprocessing.py
@@ -7,17 +7,17 @@ from dataPreprocessing.cohortney import load_data, arr_func, multiclass_fws_arra
 
 import torch
 
-def get_features(path, ext, datetime, gamma= 1.04, n_gamma_steps = 10, n_partitions = 5):
+def get_features(path, ext, datetime, gamma= 1.04, n_gamma_steps = 10, n_partitions = 10):
     ss, Ts, class2idx, user_list = load_data(path, ext = ext, datetime = datetime)
     T_h = max(Ts)
     X = []
+    steps = []
     for i in range(n_gamma_steps-1, -1, -1):
-        
         T_j = T_h/gamma**i
         for j in range(n_partitions):
-            print(T_j, j)
-            delta_T = torch.arange(0, T_j, T_j/2**j)
+            delta_T = torch.range(0, T_j, T_j/2**j)
+            steps.append(len(delta_T)-1)
             _,  events_fws_mc = arr_func(user_list, 300, delta_T, multiclass_fws_array)
             mc_batch = events_tensor(events_fws_mc)
             X.append(mc_batch)
-    return X
+    return X, steps
